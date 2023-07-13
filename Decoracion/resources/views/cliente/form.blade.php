4170922c900364b794d3162b30f5c2299a8234ac
Josephrv456 <josepovallie46@outlook.com>
2023-07-12 18:58:55 -0700

    commit para cambios

---
+++ Decoracion/resources/views/cliente/form.blade.php
@@ -0,0 +1,29 @@
+<div class="box box-info padding-1">
+    <div class="box-body">
+        
+        <div class="form-group">
+            {{ Form::label('nombre') }}
+            {{ Form::text('nombre', $cliente->nombre, ['class' => 'form-control' . ($errors->has('nombre') ? ' is-invalid' : ''), 'placeholder' => 'Nombre']) }}
+            {!! $errors->first('nombre', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('teléfono') }}
+            {{ Form::text('telefono', $cliente->telefono, ['class' => 'form-control' . ($errors->has('telefono') ? ' is-invalid' : ''), 'placeholder' => 'Teléfono']) }}
+            {!! $errors->first('telefono', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('email') }}
+            {{ Form::text('email', $cliente->email, ['class' => 'form-control' . ($errors->has('email') ? ' is-invalid' : ''), 'placeholder' => 'Email']) }}
+            {!! $errors->first('email', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+        <div class="form-group">
+            {{ Form::label('dirección') }}
+            {{ Form::text('direccion', $cliente->direccion, ['class' => 'form-control' . ($errors->has('direccion') ? ' is-invalid' : ''), 'placeholder' => 'Dirección']) }}
+            {!! $errors->first('direccion', '<div class="invalid-feedback">:message</div>') !!}
+        </div>
+
+    </div>
+    <div class="box-footer mt20">
+        <button type="submit" class="btn btn-primary">Aceptar</button>
+    </div>
+</div>
---
+++ Decoracion/resources/views/maderas.blade.php
@@ -0,0 +1,208 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+<link rel="stylesheet" type="text/css" href="{!! asset('CSS/seccion_maderas.css')!!}">
+@extends('plantilla')
+
+@section('seccion')
+		<body>
+			
+    		 <ul class="breadcrumbs">
+        <li class="breadcrumb-item">
+            <a href="/" class="breadcrumbs__link">HOME</a>
+         </li>
+         <li class="breadcrumb-item">
+            <a href="/maderas" class="breadcrumbs__link breadcrumbs__link--active">CATALOGO MDF</a>
+         </li>         
+      </ul>
+
+    		</nav>
+
+			<main class="container">
+				<div class="row">
+					<div class="col s12 center-aling">
+						<h1 class="titulo">CATÁLOGO DE MDF</h1>
+						<h3 class="subtitulo">LINEA NATURAL</h3>
+						<p>Texturas y Sensaciones que nos conectan con al Naturaleza.
+							Diseños que rescatan la naturalidad de la madera ofreciendo toques de sofisticación y elegancia.</p>
+					</div>
+				</div>
+
+				<div class="row galeria">
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/americano.PNG" class="responsive-img materialboxed" alt="" data-caption="ROBLE AMERICANO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/carvalho.PNG" class="responsive-img materialboxed" alt="" data-caption="CARVALHO ASERRADO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/roble.jpg" class="responsive-img materialboxed" alt="" data-caption="ROBLE DAKAR">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/nocce.PNG" class="responsive-img materialboxed" alt="" data-caption="NOCCE MILANO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/nogal.PNG" class="responsive-img materialboxed" alt="" data-caption="NOGAL TERRACOTA">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/teka-art.jpg" class="responsive-img materialboxed" alt="" data-caption="TEKA ARTICO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/terrarum.PNG" class="responsive-img materialboxed" alt="" data-caption="TERRARUM">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/linoza.PNG" class="responsive-img materialboxed" alt="" data-caption="LINOZA CENIZA">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/moscato.jpg" class="responsive-img materialboxed" alt="" data-caption="MOSCATO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/expresso.jpg" class="responsive-img materialboxed" alt="" data-caption="EXPRESSO">
+						</div>
+					</div>
+
+					<div class="row">
+					<div class="col s12 center-aling">
+						<h3 class="subtitulo">LINEA HILADOS</h3>
+						<p>Dedicada línea que inspira calidez y confort en sus ambientes. Sus diseños reproducen tejidos y fibras naturales con una exclusiva textura perceptible a la vista y el tacto.</p>
+					</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/seda.PNG" class="responsive-img materialboxed" alt="" data-caption="SEDA GIORNO">
+						</div>
+					</div>
+
+					<div class="row">
+					<div class="col s12 center-aling">
+						<h3 class="subtitulo">LINEA CLASSIC</h3>
+						<p>La calidez tradicional de la madera.
+						Las maderas han sido siempre parte de nuestras vidas, nos llenan de recuerdos y sensaciones gratas.</p>
+					</div>
+					</div>
+
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/cedro.PNG" class="responsive-img materialboxed" alt="" data-caption="CEDRO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/cerezo.PNG" class="responsive-img materialboxed" alt="" data-caption="CEREZO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/ebano.PNG" class="responsive-img materialboxed" alt="" data-caption="EBANO NEGRO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/haya.PNG" class="responsive-img materialboxed" alt="" data-caption="HAYA">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/maple.PNG" class="responsive-img materialboxed" alt="" data-caption="MAPLE">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/wengue.PNG" class="responsive-img materialboxed" alt="" data-caption="WENGUE">
+						</div>
+					</div>
+
+					<div class="row">
+					<div class="col s12 center-aling">
+						<h3 class="subtitulo">LINEA NÓRDICA</h3>
+						<p>Textura exclusiva inspirada en madera nativa, con poros suaves y elegantes.
+						Aspecto atemporal, a la vez ancestral y futurista  y sencillez rústica son las características de los diseños que la conforman, de carácter cálido, sencillo y minimalista.</p>
+					</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/helsinki.PNG" class="responsive-img materialboxed" alt="" data-caption="NOGAL TERRACOTA">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/olmo.PNG" class="responsive-img materialboxed" alt="" data-caption="OLMO FINLANDÉS">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/baltico.PNG" class="responsive-img materialboxed" alt="" data-caption="BÁLTICO">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/escandinavo.PNG" class="responsive-img materialboxed" alt="" data-caption="ESCANDINAVO">
+						</div>
+					</div>
+
+
+					<div class="row">
+					<div class="col s12 center-aling">
+						<h3 class="subtitulo">LINEA MATERIA</h3>
+						<p>Una textura innovadora que transmite diferentes sensaciones al aplicarse a elementos rústicos, naturales, industriales o textiles</p>
+					</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/concreto.jpg" class="responsive-img materialboxed" alt="" data-caption="CONCRETO METROPOLITAN">
+						</div>
+					</div>
+
+					<div class="col s12 m4 l3">
+						<div class="material-placeholder">
+							<img src="IMG/melaminas/nebraska.jpg" class="responsive-img materialboxed" alt="" data-caption="NEBRASKA">
+						</div>
+					</div>
+
+					
+				</div>
+			</main>
+
+
+			<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+			<script type="text/javascript" src="js/main.js"></script>
+		</body>	
+@endsection
